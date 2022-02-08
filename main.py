1153b9f5ac38099c9d958883b4bc65ef4a990075
Nadie <96995451+NadieSpain@users.noreply.github.com>
2022-02-08 19:46:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
-iimport pygame 
+import pygame 
 from pygame.locals import *
 import numpy as np 
 import time
 import sys
 import random
-import pickle
+from datetime import datetime
+
+__Version__=1.21
+__Authors__=["NadieSpain", "github.com","/NadieSpain",""]
 
 
-icon = pygame.image.load("game.png")
+
+icon = pygame.image.load("game3.png")
 pygame.display.set_icon(icon)
 
-pygame.display.set_caption("Conway 's Game of Life")
+pygame.display.set_caption("Conway's Game of Life")
 
 gameOver=False
 
 boolgrid=True
-boolinfo=False
+boolinfo=True
 
 sum_generation=0
-__generation__=0
+Generation=0
 
 list_generation=[]
 pygame.init()
 
-width, height = 750, 750
-screen = pygame.display.set_mode((height, width))
+
+grid_width, grid_height = 750, 750
+
+
+width, height = 950, 750
+screen = pygame.display.set_mode((width,height), pygame.DOUBLEBUF)
 
 black=0,0,0
 white=255,255,255
-grey=128,128,128
-blue= 0,100,150
-yellow=255, 233, 0
+
 
 bg= black
 bg_cube=white
-bg_grid=grey
-bg_font=yellow
 
 screen.fill(bg)
 
 nxC, nyC=100, 100
 
-dimCW=width/nxC
-dimCH=height/nyC
+dimCW=grid_width/nxC
+dimCH=grid_height/nyC
 
 gameState=np.zeros((nxC,nyC))
+
+
+fontx, fonty= width-150, 25
 
 #automata palo
 """
@@ -52,7 +59,19 @@ gameState[5,3]=1
 gameState[5,4]=1
 gameState[5,5]=1
 """
+def pause():
+	global pauseExect
+	pauseExect= not pauseExect
 
+def mode():
+	global bg
+	global bg_cube
+	if bg==black:
+		bg=white
+		bg_cube=black
+	else:
+		bg=black
+		bg_cube=white
 
 
 def save():
@@ -66,20 +85,24 @@ def save():
 
 
 def show_generation():
-	print(__generation__)
+	print(Generation)
 
 def reset():
-	global __generation__
-	__generation__= 0
+	global Generation
+	global pauseExect
+	pE=pauseExect
+	pauseExect=True
+	Generation= 0
 	for y in range(0, nxC):
 		for x in range(0, nyC):
 			newGameState[x,y]=0
+	pauseExect=pE
 
 def next_poblation():
 	global gameState
 	global newGameState
 	global sum_generation
-	global __generation__
+	global Generation
 
 	sum_generation=0
 	for y in range(0, nxC):
@@ -105,38 +128,56 @@ def next_poblation():
 
 	gameState =np.copy(newGameState)
 	if sum_generation!=0 and pauseExect:
-		__generation__+=1
+		Generation+=1
 
 
 
-pauseExect= False
+pauseExect = False
+
 
 while not gameOver:
-	if boolinfo:
-		fuente = pygame.font.SysFont("Terminal", 50)
-		mensaje= fuente.render("Grid: {}x{}".format(nxC,nyC), 1, bg_font)
-		screen.blit(mensaje, (10, 10))
-		pygame.display.flip()
 
-		mensaje= fuente.render("Generation: "+str(__generation__), 1, bg_font)
-		screen.blit(mensaje, (width-250-(len(str(__generation__))*20), height-50))
-		pygame.display.flip()
+	if boolinfo:	
+	
+		fuente = pygame.font.SysFont("Small Fonts Normal", 25)
+		mensaje= fuente.render("Grid: {}x{}".format(nxC,nyC), 1, bg_cube)
+		screen.blit(mensaje, (fontx, fonty*1))
+	
+		mensaje= fuente.render("Generation: "+str(Generation), 1, bg_cube)
+		screen.blit(mensaje, (fontx, fonty*2))
 
-		mensaje= fuente.render("Poblation: "+str(round(sum_generation)), 1, bg_font)
-		screen.blit(mensaje, (10, height-50))
-		pygame.display.flip()
+		mensaje= fuente.render("Poblation: "+str(round(sum_generation)), 1, bg_cube)
+		screen.blit(mensaje, (fontx, fonty*3))
+
+		mensaje= fuente.render("Pause" if pauseExect else "Running", 1, (bg_cube if not pauseExect else (255,0,0)))
+		screen.blit(mensaje, (fontx, fonty*4))
+		
+	else:
+		
+		fuente = pygame.font.SysFont("Emulogic", 25)
+		mensaje= fuente.render("Created by", 1, bg_cube)
+		screen.blit(mensaje, (fontx, fonty*1))
+		for i in range(len(__Authors__)):
+	
+			mensaje= fuente.render(__Authors__[i], 1, bg_cube)
+			screen.blit(mensaje, (fontx, fonty*(i+2)))
+			t=i
+
+		mensaje= fuente.render("Version: {}".format(__Version__), 1, bg_cube)
+		screen.blit(mensaje, (fontx, fonty*(t+3)))
+
+	pygame.display.flip()
+
 
 
 	newGameState=np.copy(gameState)
 
 	screen.fill(bg)
-
-	time.sleep(0.1)
+	time.sleep(0.01)
 
 	
-
 	for event in pygame.event.get():
-		keys_pressed= pygame.key.get_pressed()
+		keys_pressed = pygame.key.get_pressed()
 		if event.type == pygame.QUIT:
 			sys.exit()
 		if  event.type ==pygame.KEYDOWN:
@@ -146,8 +187,8 @@ while not gameOver:
 				if nxC>=20:
 					nxC-=10
 					nyC-=10
-					dimCW=width/nxC
-					dimCH=height/nyC
+					dimCW=grid_width/nxC
+					dimCH=grid_height/nyC
 					newGameState=np.zeros((nxC,nyC))
 					for i in r_l:
 						try:
@@ -159,8 +200,8 @@ while not gameOver:
 				if nxC<=110:
 					nxC+=10
 					nyC+=10
-					dimCW=width/nxC
-					dimCH=height/nyC
+					dimCW=grid_width/nxC
+					dimCH=grid_height/nyC
 					newGameState=np.zeros((nxC,nyC))
 					gameState=np.zeros((nxC,nyC))
 					for i in r_l:
@@ -168,35 +209,34 @@ while not gameOver:
 							newGameState[i[0],i[1]]=1
 						except:pass
 			if event.key==pygame.K_m:
-				if bg==black:
-					bg=white
-					bg_cube=black
-					bg_grid=blue
-					bg_font=grey
-				else:
-					bg=black
-					bg_cube=white
-					bg_grid=grey
-					bg_font=yellow
+				mode()
 			if event.key==pygame.K_k:
-				pauseExect= not pauseExect
-			if event.key==pygame.K_r:
+				pause()
+			if event.key==pygame.K_r or event.key==pygame.K_c:
 				reset()
 			if event.key==pygame.K_g:
 				boolgrid= not boolgrid 
-			if event.key==pygame.K_s:
-				#save()
-				show_generation()
-			if event.key==pygame.K_n and pauseExect:
-					next_poblation()
 			if event.key==pygame.K_i:
 				boolinfo= not boolinfo
-		mouseClick=pygame.mouse.get_pressed()
+				
+			if event.key==pygame.K_n and pauseExect:
+
+					next_poblation()
+
+
+		
+		mouseClick=pygame.mouse.get_pressed()		
 		if sum(mouseClick)>0:
 			posX, posY= pygame.mouse.get_pos()
-
 			celX, celY= int(np.floor(posX/dimCW)), int(np.floor(posY/dimCH))
-			newGameState[celX,celY]= not mouseClick[2]
+			try:
+				newGameState[celX,celY]= not mouseClick[2]
+			except: 
+				
+				if 770<posX<=780 and 50<posY<=60 and pauseExect:
+
+					next_poblation()
+
 
 	for y in range(0, nxC):
 		for x in range(0, nyC):
@@ -222,26 +262,34 @@ while not gameOver:
 					((x+1) * dimCW, (y+1) * dimCH),
 					((x) * dimCW, (y+1) * dimCH)]
 
-			if newGameState[x,y]== 0 and boolgrid:
-				pygame.draw.polygon(screen,bg_grid, poly ,1)
+			lines_list=[(0,0),(grid_width,0), (grid_width,height-1), (0, height-1), (0,0)]
+			lines_list2=[(grid_width-1,0), (width-1,0), (width-1, height-1), (grid_width-1, height-1)]
+			if newGameState[x,y]== 0:
+				if boolgrid:
+					pygame.draw.polygon(screen,bg_cube, poly ,1)
+
+				
 			elif newGameState[x,y]== 1:
 				pygame.draw.polygon(screen,bg_cube, poly ,0)
 
 
+	
 	before_list_generation=list_generation
 	list_generation=save()
 	before_sum_generation=sum_generation
 	sum_generation=0
+
+	if not boolgrid:
+		pygame.draw.lines (screen, bg_cube, False,  [(grid_width, 0),(grid_width,grid_height)], 2)
 	for y in range(0, nxC):
 		for x in range(0, nyC):
 			sum_generation+=newGameState[x,y]
 
+
 	if sum_generation>0 and not pauseExect and before_list_generation!=list_generation:
-		__generation__+=1
+		Generation+=1
 	elif sum_generation>0 and before_sum_generation==0:
-		__generation__=0
-	elif list_generation==before_list_generation:
-		pass
+		Generation=0
 
 	gameState =np.copy(newGameState)
 	
